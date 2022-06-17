bab26b2b88edbeda3089fd622cba405f20b573eb
karan sivarat <bomb0069@gmail.com>
2022-06-17 18:59:49 +0700

    [Disabled]: Internet Gateway

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -23,6 +23,7 @@ module "vpc" {
   enable_nat_gateway   = false
   single_nat_gateway   = false
   enable_dns_hostnames = false
+  create_igw           = false
 
   private_subnet_tags = {
     subnet-type = "application"
